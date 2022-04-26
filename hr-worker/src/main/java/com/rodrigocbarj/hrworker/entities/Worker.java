f20e89da0f0020be9c10612e4cdaf792d287e3aa
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-04-26 17:57:45 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/rodrigocbarj/hrworker/entities/Worker.java
@@ -0,0 +1,49 @@
+package com.rodrigocbarj.hrworker.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_worker")
+public class Worker implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Worker other = (Worker) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+}
---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Rodrigo', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Manuella', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Nelio', 250.0);
