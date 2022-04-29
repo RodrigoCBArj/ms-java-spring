95ad40eee40321627922d232dacc0fea7e47152f
RodrigoCBArj <rodrigocbarj@gmail.com>
2022-04-29 18:40:32 -0300

    UserRepository, UserResource, Zuul config

---
+++ hr-user/src/main/java/com/rodrigocbarj/hruser/resources/UserResource.java
@@ -0,0 +1,27 @@
+package com.rodrigocbarj.hruser.resources;
+
+import com.rodrigocbarj.hruser.entities.User;
+import com.rodrigocbarj.hruser.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = repository.findById(id).get();
+        return ResponseEntity.ok(user);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<User> findById(@RequestParam String email) {
+        User user = repository.findByEmail(email);
+        return ResponseEntity.ok(user);
+    }
+}
